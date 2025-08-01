832b56f3376e6a810201e6bf942255b83b12df61
voodoo-behavior <voodoo.behavior@gmail.com>
2025-08-01 15:06:02 +0300

    Практикуемся в реализации PageComponent. Реализация и использование компонента

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -11,7 +11,6 @@ from pages.registration_page import RegistrationPage
 @pytest.mark.courses
 @pytest.mark.regression
 def test_empty_courses_list(courses_list_page: CoursesListPage):
-
     courses_list_page.visit(
         "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses"
     )
@@ -19,8 +18,7 @@ def test_empty_courses_list(courses_list_page: CoursesListPage):
     courses_list_page.navbar.check_visible('username')
     courses_list_page.sidebar.check_visible()
 
-    courses_list_page.check_visible_courses_title()
-    courses_list_page.check_visible_create_course_button()
+    courses_list_page.toolbar_view.check_visible()
     courses_list_page.check_visible_empty_view()
 
 
@@ -38,58 +36,38 @@ def test_wrong_email_or_password_authorization(login_page: LoginPage, email: str
 
 @pytest.mark.courses
 @pytest.mark.regression
-@pytest.mark.parametrize("title, description, max_score, min_score, estimated_time, file_path", [
-    (
-            "Playwright",
-            "Playwright",
-            "100", "10",
-            "2 weeks",
-            "./testdata/files/image.png"
-    )
-])
-def test_create_course(
-        create_course_page: CreateCoursePage,
-        courses_list_page: CoursesListPage,
-        title: str,
-        description: str,
-        max_score: str,
-        min_score: str,
-        estimated_time: str,
-        file_path: str
-):
+def test_create_course(create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
     create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
 
     # Check 'Create course' page has the default state
     create_course_page.check_visible_create_course_title()
     create_course_page.check_disabled_create_course_button()
-    create_course_page.check_visible_image_preview_empty_view()
-    create_course_page.check_visible_image_upload_view()
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
     create_course_page.check_visible_create_course_form(
-        '', '', '', '0', '0'
+        title='', description='', estimated_time='', min_score='0', max_score='0'
     )
     create_course_page.check_visible_exercises_title()
     create_course_page.check_visible_create_exercise_button()
     create_course_page.check_visible_exercises_empty_view()
 
     # Fill Course data and submit for adding
-    create_course_page.upload_preview_image(file_path)
-    create_course_page.check_visible_image_upload_view(True)
+    create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
     create_course_page.fill_create_course_form(
-        title,
-        estimated_time,
-        description,
-        max_score,
-        min_score
+        title="Playwright",
+        estimated_time="2 weeks",
+        description="Playwright",
+        max_score="100",
+        min_score="10"
     )
     create_course_page.click_create_course_button()
 
     # Check created Course is in the list and with the proper data
-    courses_list_page.check_visible_courses_title()
-    courses_list_page.check_visible_create_course_button()
-    courses_list_page.check_visible_course_card(
-        0,
-        title,
-        max_score,
-        min_score,
-        estimated_time
+    courses_list_page.toolbar_view.check_visible()
+    courses_list_page.course_view.check_visible(
+        index=0,
+        title="Playwright",
+        max_score="100",
+        min_score="10",
+        estimated_time="2 weeks"
     )
