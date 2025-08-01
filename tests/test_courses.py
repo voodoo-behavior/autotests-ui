8923b220278b56aa9ef727ed4723149080743f89
voodoo-behavior <voodoo.behavior@gmail.com>
2025-08-01 19:17:02 +0300

    Практикуемся в реализации PageComponent. Практикуемся в написании компонентов с использованием паттерна PageComponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,11 +1,8 @@
 import pytest
-from playwright.sync_api import expect, Page
 
 from pages.courses_list_page import CoursesListPage
 from pages.create_course_page import CreateCoursePage
-from pages.dashboard_page import DashboardPage
 from pages.login_page import LoginPage
-from pages.registration_page import RegistrationPage
 
 
 @pytest.mark.courses
@@ -22,6 +19,8 @@ def test_empty_courses_list(courses_list_page: CoursesListPage):
     courses_list_page.check_visible_empty_view()
 
 
+@pytest.mark.regression
+@pytest.mark.registration
 @pytest.mark.parametrize("email, password", [
     ("user.name@gmail.com", "password"),
     ("user.name@gmail.com", '  '),
@@ -29,7 +28,8 @@ def test_empty_courses_list(courses_list_page: CoursesListPage):
 ])
 def test_wrong_email_or_password_authorization(login_page: LoginPage, email: str, password: str):
     login_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
-    login_page.fill_login_form(email, password)
+    login_page.login_form.check_visible(email="",password="")
+    login_page.login_form.fill(email, password)
     login_page.click_login_button()
     login_page.check_visible_wrong_email_or_password_alert()
 
@@ -40,27 +40,25 @@ def test_create_course(create_course_page: CreateCoursePage, courses_list_page: 
     create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
 
     # Check 'Create course' page has the default state
-    create_course_page.check_visible_create_course_title()
-    create_course_page.check_disabled_create_course_button()
+    create_course_page.create_course_toolbar_view.check_visible(is_create_course_disabled=True)
     create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
-    create_course_page.check_visible_create_course_form(
+    create_course_page.create_course_form.check_visible(
         title='', description='', estimated_time='', min_score='0', max_score='0'
     )
-    create_course_page.check_visible_exercises_title()
-    create_course_page.check_visible_create_exercise_button()
+    create_course_page.create_course_exercises_toolbar_view.check_visible()
     create_course_page.check_visible_exercises_empty_view()
 
     # Fill Course data and submit for adding
     create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
     create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
-    create_course_page.fill_create_course_form(
+    create_course_page.create_course_form.fill(
         title="Playwright",
         estimated_time="2 weeks",
         description="Playwright",
         max_score="100",
         min_score="10"
     )
-    create_course_page.click_create_course_button()
+    create_course_page.create_course_toolbar_view.click_create_course_button()
 
     # Check created Course is in the list and with the proper data
     courses_list_page.toolbar_view.check_visible()
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -6,17 +6,20 @@ from pages.registration_page import RegistrationPage
 
 @pytest.mark.regression
 @pytest.mark.registration
-def test_successful_registration(
-        registration_page: RegistrationPage,
-        dashboard_page: DashboardPage
-):
+def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage):
     registration_page.visit(
         "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
-    registration_page.fill_registration_form(
+    registration_page.registration_form.check_visible(
+        email="",
+        username="",
+        password="",
+    )
+
+    registration_page.registration_form.fill(
         email="user.name@gmail.com",
         username="username",
         password="password"
     )
     registration_page.click_registration_button()
-    dashboard_page.check_visible_dashboard_title()
+    dashboard_page.dashboard_toolbar_view.check_visible()
