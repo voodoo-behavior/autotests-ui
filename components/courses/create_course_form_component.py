9134555b89654de82d5d05ec7070b6cbe87c4642
voodoo-behavior <voodoo.behavior@gmail.com>
2025-08-02 16:19:48 +0300

    Практические применение PageFactory в автотестах. Практикуемся в применении Page Factory

--- components/courses/create_course_form_component.py
+++ components/courses/create_course_form_component.py
@@ -1,21 +1,24 @@
 from playwright.sync_api import Page, expect
 
 from components.base_component import BaseComponent
+from elements.input import Input
+from elements.textarea import TextArea
 
 
 class CreateCourseFormComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.max_score_input = page.get_by_test_id('create-course-form-max-score-input').locator('input')
-        self.min_score_input = page.get_by_test_id('create-course-form-min-score-input').locator('input')
+        self.max_score_input = Input(page, 'create-course-form-max-score-input', 'Max Score')
+        self.min_score_input = Input(page, 'create-course-form-min-score-input', 'Min Score')
 
-        self.title_input = page.get_by_test_id('create-course-form-title-input').locator('input')
+        self.title_input = Input(page, 'create-course-form-title-input', 'Title')
+
         self.estimated_time_input = (
-            page.get_by_test_id('create-course-form-estimated-time-input').locator('input')
+            Input(page, 'create-course-form-estimated-time-input', 'Estimated Time')
         )
         self.description_textarea = (
-            page.get_by_test_id('create-course-form-description-input').locator('textarea').first
+            TextArea(page, 'create-course-form-description-input', 'Description')
         )
 
     def check_visible(
@@ -26,20 +29,20 @@ class CreateCourseFormComponent(BaseComponent):
             max_score: str,
             min_score: str
     ):
-        expect(self.title_input).to_be_visible()
-        expect(self.title_input).to_have_value(title)
+        self.title_input.check_visible()
+        self.title_input.check_have_value(title)
 
-        expect(self.estimated_time_input).to_be_visible()
-        expect(self.estimated_time_input).to_have_value(estimated_time)
+        self.estimated_time_input.check_visible()
+        self.estimated_time_input.check_have_value(estimated_time)
 
-        expect(self.description_textarea).to_be_visible()
-        expect(self.description_textarea).to_have_value(description)
+        self.description_textarea.check_visible()
+        self.description_textarea.check_have_value(description)
 
-        expect(self.max_score_input).to_be_visible()
-        expect(self.max_score_input).to_have_value(max_score)
+        self.max_score_input.check_visible()
+        self.max_score_input.check_have_value(max_score)
 
-        expect(self.min_score_input).to_be_visible()
-        expect(self.min_score_input).to_have_value(min_score)
+        self.min_score_input.check_visible()
+        self.min_score_input.check_have_value(min_score)
 
     def fill(
             self,
@@ -50,16 +53,16 @@ class CreateCourseFormComponent(BaseComponent):
             min_score: str
     ):
         self.title_input.fill(title)
-        expect(self.title_input).to_have_value(title)
+        self.title_input.check_have_value(title)
 
         self.estimated_time_input.fill(estimated_time)
-        expect(self.estimated_time_input).to_have_value(estimated_time)
+        self.estimated_time_input.check_have_value(estimated_time)
 
         self.description_textarea.fill(description)
-        expect(self.description_textarea).to_have_value(description)
+        self.description_textarea.check_have_value(description)
 
         self.max_score_input.fill(max_score)
-        expect(self.max_score_input).to_have_value(max_score)
+        self.max_score_input.check_have_value(max_score)
 
         self.min_score_input.fill(min_score)
-        expect(self.min_score_input).to_have_value(min_score)
+        self.min_score_input.check_have_value(min_score)
